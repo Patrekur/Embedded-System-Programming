27a12f0431a06ac2acfa1327000913c2eb78fb84
Patrekur <31309672+Patrekur@users.noreply.github.com>
2020-09-12 15:17:03 +0000

    Create digital_in.h

---
+++ Project 2 MultiTasking/src/digital_in.h
@@ -0,0 +1,15 @@
+#include <arduino.h> 
+
+class Digital_in {
+public:
+    
+    Digital_in(int pin);
+    void init();
+    bool is_hi();
+    bool is_lo();
+
+private:
+
+    uint8_t pinMask;
+
+};
