c894be5e654c82c752d5006e43b13519c3a37697
Patrekur <31309672+Patrekur@users.noreply.github.com>
2020-09-12 21:28:05 +0000

    Update analog_out.cpp

--- Project 2 MultiTasking/src/analog_out.cpp
+++ Project 2 MultiTasking/src/analog_out.cpp
@@ -3,6 +3,8 @@
 Analog_out::Analog_out(float duty) {
 
     dutyCycle = duty;
+    cmprcount = 16000000/256 - 1;
+    bool mode = false;
 
 }
 
@@ -11,28 +13,50 @@ void Analog_out::init() {
     // Timer counter control registers set to 0
     TCCR1A = 0; 
     TCCR1B = 0;
-    // An arbitrary value to create a uneven on/off time
+    // Value of compare register B
     OCR1B = cmprcount*dutyCycle;
-    // With a prescaler of 1024 the corresponding value in the output compare register for 1 second
-    // Duty cycle is then: (15625-3200)/15625 = 79,5 %
+    // Value of compare register A
     OCR1A = cmprcount;
     // Clear timer on compare 
     TCCR1B |= (1 << WGM12); 
     // set interrupt on compare match A and B
     TIMSK1 |= (1 << OCIE1A) | (1 << OCIE0B);
-    // set prescaler to 1024 and start the timer
-    TCCR1B |= (1 << CS12) | (1 << CS10);
-    // Global interrupts enabled
+    // set prescaler to 256 and start the timer
+    TCCR1B |= (1 << CS12);
 
 }
 
 void Analog_out::dutyset(float duty) {
 
-    Serial.println((int)(duty*cmprcount));
+   // Serial.println((int)(duty*cmprcount));
 
     dutyCycle = duty;
     OCR1B = (int)(cmprcount*dutyCycle);
 
+}
+
+void Analog_out::changemode() {
+
+if (mode == false) {
+
+    // Counter changed to a low level to provide a high PWM frequency
+    unsigned int cmprcount = 30;
+    OCR1A = cmprcount;
+    OCR1B = cmprcount * dutyCycle;
+    TCNT1 = 0;
+    mode = true;
+
+}
+
+else {
+
+    unsigned int cmprcount = 16000000/256 - 1;
+    OCR1A = cmprcount;
+    OCR1B = cmprcount * dutyCycle;
+    TCNT1 = 0;
+    mode = false;
+
+}
 
 
 }
