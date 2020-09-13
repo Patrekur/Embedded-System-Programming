66377fef1e8ffeb6e843b45dcec141a13615fd4a
Patrekur <31309672+Patrekur@users.noreply.github.com>
2020-09-13 00:03:04 +0000

    Update main.cpp

--- Project 2 MultiTasking/src/main.cpp
+++ Project 2 MultiTasking/src/main.cpp
@@ -1,89 +1,75 @@
-#include <arduino.h>
-#include <math.h>
-#include "digital_out.h"
-#include "digital_in.h"
-#include "analog_in.h"
-#include "analog_out.h"
+// #include <arduino.h>
+// #include "digital_out.h"
+// #include "digital_in.h"
+// #include "analog_in.h"
+// #include "analog_out.h"
 
-Digital_out digiout(5);
-Digital_in  digiin(2);
-Analog_in analogIn(0);
-Analog_out analogOut(0.5);
-float val = 0;
-
-double mapOneRangeToAnother(double sourceNumber, double fromA, double fromB, double toA, double toB, int decimalPrecision ) {
-    double deltaA = fromB - fromA;
-    double deltaB = toB - toA;
-    double scale  = deltaB / deltaA;
-    double negA   = -1 * fromA;
-    double offset = (negA * scale) + toA;
-    double finalNumber = (sourceNumber * scale) + offset;
-    int calcScale = (int) pow(10, decimalPrecision);
-    return (double) round(finalNumber * calcScale) / calcScale;
-}
-
-void setup() {
+// Digital_out digiout(5);
+// Digital_in  digiin(2);
+// Analog_in analogIn(0);
+// Analog_out analogOut(0.5);
+// unsigned int val = 0;
 
 
-digiout.init();
-digiin.init();
-analogIn.init();
-analogOut.init();
-sei(); 
-Serial.begin(9600); 
-}
-
-void loop() {
-
-
-  
-  
-  val = analogIn.getval();
-  Serial.print("val fyrir:    ");Serial.println(val);
-
-  val = map(val, 0, 400, 1, 255);
-  
+// double mapOneRangeToAnother(double sourceNumber, double fromA, double fromB, double toA, double toB, int decimalPrecision ) {
+//     double deltaA = fromB - fromA;
+//     double deltaB = toB - toA;
+//     double scale  = deltaB / deltaA;
+//     double negA   = -1 * fromA;
+//     double offset = (negA * scale) + toA;
+//     double finalNumber = (sourceNumber * scale) + offset;
+//     int calcScale = (int) pow(10, decimalPrecision);
+//     return (double) round(finalNumber * calcScale) / calcScale;
+// }
 
 
-  Serial.print("mapped val:           ");Serial.println(val);
-  
-  val = val/(float)255;
-  Serial.print("val/255: between 0-1: ");Serial.println(val);
-  double myResult = (double) mapOneRangeToAnother(val, 0.15, 0.6, 0, 1, 3);
-  Serial.print("mapOneRangeToAnother: ");Serial.println(myResult);
-  //val = map(val,0.25,0.55,0,1);
-  //Serial.print("map(val,0.25,0.55,0,1): ");Serial.println(val);
+// void setup() {
 
-  analogOut.dutyset(myResult);
-  // analogOut.dutyset(val/(float)255);
-  delay(250);
+// digiout.init();
+// digiin.init();
+// analogIn.init();
+// analogOut.init();
+// sei(); 
+// Serial.begin(9600);
+// analogOut.changemode();
+// }
 
-}
+// void loop() {
 
-// Interrupt routines
+//   //val = map(analogIn.getval(), 40, 200, 1, 255);
+//   double val = analogIn.getval();
+//   Serial.print("unmapped: ");Serial.println(val);
 
-ISR(INT0_vect) {
+//   val = (double) mapOneRangeToAnother(val, 9, 60, 0, 1, 3);
+//   Serial.println(val);
+//   analogOut.dutyset(val);
 
-  Serial.println("Interrupt!!");
-  delay(50);
-  analogOut.changemode();
+// }
 
-  // The ISR runs while the button is held
-  while(digiin.is_lo()) {}
+// // Interrupt routines
 
+// ISR(INT0_vect) {
 
-}
+//   cli();
 
-ISR(TIMER1_COMPB_vect)
-{
+//   Serial.println("Interrupt!!");
 
-  digiout.set_lo();
+//   analogOut.changemode();
 
-}
+//   sei();
 
-ISR(TIMER1_COMPA_vect)
-{
+// }
 
-  digiout.set_hi();
+// ISR(TIMER1_COMPB_vect)
+// {
 
-}
+//   digiout.set_lo();
+
+// }
+
+// ISR(TIMER1_COMPA_vect)
+// {
+
+//   digiout.set_hi();
+
+// }
