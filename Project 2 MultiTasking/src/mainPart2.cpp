2d734926df6a3cb4ab70c15365b6f380453dacd7
Patrekur <31309672+Patrekur@users.noreply.github.com>
2020-09-13 00:58:51 +0000

    Create mainPart2.cpp

---
+++ Project 2 MultiTasking/src/mainPart2.cpp
@@ -0,0 +1,91 @@
+
+
+// This main.cpp file contains The filtering part of part 2 in project 2. 
+
+
+#include <arduino.h>
+#include <math.h>
+#include "digital_out.h"
+#include "digital_in.h"
+#include "analog_in.h"
+#include "analog_out.h"
+#include "filter.h"
+#include "debounce.h"
+
+Digital_out digiout(5);
+Digital_in  digiin(2);
+Analog_in analogIn(0);
+Analog_out analogOut(0.5);
+Debounce debon;
+IIR_Filter IIR(0.4); // input is alpha // 0.2 = 20% weight.  0 < alpha < 1
+// low alpha means good smoothing but slow response, and vice versa.
+
+float val = 0;
+double IIRVal = 0;
+
+double mapOneRangeToAnother(double sourceNumber, double fromA, double fromB, double toA, double toB, int decimalPrecision ) {
+    // https://stackoverflow.com/questions/5731863/mapping-a-numeric-range-onto-another
+    // We needed to map analog sensor value to its true potential range of 0-255
+    // to be able to then use it to affect the duty cycle.
+    // This custom function, unlike the map() function from the arduino library, returns double values. 
+    if (sourceNumber > fromB){fromB = sourceNumber;}
+    if (sourceNumber < fromA){fromA = sourceNumber;}
+    double deltaA = fromB - fromA;
+    double deltaB = toB - toA;
+    double scale  = deltaB / deltaA;
+    double negA   = -1 * fromA;
+    double offset = (negA * scale) + toA;
+    double finalNumber = (sourceNumber * scale) + offset;
+    int calcScale = (int) pow(10, decimalPrecision);
+    return (double) round(finalNumber * calcScale) / calcScale;
+}
+
+
+void setup() {
+
+
+digiout.init();
+digiin.init();
+analogIn.init();
+analogOut.init();
+debon.init();
+sei(); 
+Serial.begin(9600); 
+analogOut.changemode();
+}
+
+void loop() {
+  val = analogIn.getval(); // read the analog sensor value
+  IIRVal = IIR.exponentialFilter(val); // filter the signal 
+  double mappedVal = (double) mapOneRangeToAnother(IIRVal, 9, 90, 0, 1, 3); // map the signal
+  Serial.print("mappedValll:      ");Serial.println(mappedVal);
+  analogOut.dutyset(mappedVal); // set the duty cycle according to the signal of the sensor
+  //delay(250);
+
+}
+
+// Interrupt routines
+
+ISR(INT0_vect) {
+  cli();
+  Serial.println("Interrupt!!");
+  delay(50);
+  analogOut.changemode();
+
+  sei();
+
+}
+
+ISR(TIMER1_COMPB_vect)
+{
+
+  digiout.set_lo();
+
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+
+  digiout.set_hi();
+
+}
