ef2650db694c30d96650698f73c450553b61c3ec
Patrekur <31309672+Patrekur@users.noreply.github.com>
2020-09-18 13:11:05 +0000

    Update mainPart2Debounce.cpp

--- Project 2 MultiTasking/src/mainPart2Debounce.cpp
+++ Project 2 MultiTasking/src/mainPart2Debounce.cpp
@@ -1,98 +1,98 @@
 
 
-// // This main.cpp file contains our struggles with the debounce class 
-// // it does not function properly
+// This main.cpp file contains our struggles with the debounce class 
+// it does not function properly
 
-// #include <arduino.h>
-// #include "digital_out.h"
-// #include "digital_in.h"
-// #include "analog_in.h"
-// #include "analog_out.h"
-// #include "debounce.h"
+#include <arduino.h>
+#include "digital_out.h"
+#include "digital_in.h"
+#include "analog_in.h"
+#include "analog_out.h"
+#include "debounce.h"
 
-// Digital_out digiout(5);
-// Digital_in  digiin(2);
-// Analog_in analogIn(0);
-// Analog_out analogOut(0.5);
-// Debounce debon;
+Digital_out digiout(5);
+Digital_in  digiin(2);
+Analog_in analogIn(0);
+Analog_out analogOut(0.5);
+Debounce debon;
 
-// unsigned int val = 0;
-// double myResult = 0;
+unsigned int val = 0;
+double myResult = 0;
 
-// void setup() {
+void setup() {
 
-// digiout.init();
-// digiin.init();
-// analogIn.init();
-// analogOut.init();
-// debon.init();
-// sei(); 
-// Serial.begin(9600);
-// analogOut.changemode();
-// }
+digiout.init();
+digiin.init();
+analogIn.init();
+analogOut.init();
+debon.init();
+sei(); 
+Serial.begin(9600);
+analogOut.changemode();
+}
 
-// double mapOneRangeToAnother(double sourceNumber, double fromA, double fromB, double toA, double toB, int decimalPrecision ) {
-//     double deltaA = fromB - fromA;
-//     double deltaB = toB - toA;
-//     double scale  = deltaB / deltaA;
-//     double negA   = -1 * fromA;
-//     double offset = (negA * scale) + toA;
-//     double finalNumber = (sourceNumber * scale) + offset;
-//     int calcScale = (int) pow(10, decimalPrecision);
-//     return (double) round(finalNumber * calcScale) / calcScale;
-// }
+double mapOneRangeToAnother(double sourceNumber, double fromA, double fromB, double toA, double toB, int decimalPrecision ) {
+    double deltaA = fromB - fromA;
+    double deltaB = toB - toA;
+    double scale  = deltaB / deltaA;
+    double negA   = -1 * fromA;
+    double offset = (negA * scale) + toA;
+    double finalNumber = (sourceNumber * scale) + offset;
+    int calcScale = (int) pow(10, decimalPrecision);
+    return (double) round(finalNumber * calcScale) / calcScale;
+}
 
-// void loop() {
+void loop() {
   
-//   if (debon.ispressed()) {
+  if (debon.ispressed()) {
 
-//     analogOut.changemode();
-//     debon.reset();
+    analogOut.changemode();
+    debon.reset();
 
-//   }
+  }
 
-//   Serial.println(debon.getconsec());
+  Serial.println(debon.getconsec());
 
-//   //val = map(analogIn.getval(), 40, 200, 1, 255);
-//   val = analogIn.getval();
-//   myResult = (double) mapOneRangeToAnother(val, 10, 70, 0, 1, 1);
-//   //Serial.println(myResult);
-//   analogOut.dutyset(myResult);
+  //val = map(analogIn.getval(), 40, 200, 1, 255);
+  val = analogIn.getval();
+  myResult = (double) mapOneRangeToAnother(val, 10, 70, 0, 1, 1);
+  //Serial.println(myResult);
+  analogOut.dutyset(myResult);
 
-// }
+}
 
-// // Interrupt routines
+// Interrupt routines
 
-// /*ISR(INT0_vect) {
+/*ISR(INT0_vect) {
 
-//   cli();
+  cli();
 
-//   Serial.println("Interrupt!!");
+  Serial.println("Interrupt!!");
 
-//   analogOut.changemode();
+  analogOut.changemode();
 
-//   sei();
+  sei();
 
-// }
+}
 
-// */
+*/
 
-// ISR(TIMER1_COMPB_vect)
-// {
+ISR(TIMER1_COMPB_vect)
+{
 
-//   digiout.set_lo();
+  digiout.set_lo();
 
-// }
+}
 
-// ISR(TIMER1_COMPA_vect)
-// {
+ISR(TIMER1_COMPA_vect)
+{
 
-//   digiout.set_hi();
+  digiout.set_hi();
 
-// }
+}
 
-// ISR(TIMER2_COMPA_vect) {
+ISR(TIMER2_COMPA_vect) {
 
-//   debon.debounce(digiin.is_lo());
+  debon.debounce(digiin.is_lo());
 
-// }
+}
