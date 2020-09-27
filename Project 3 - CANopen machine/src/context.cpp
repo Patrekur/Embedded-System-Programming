915aa54aed17f86526dc99e0b57b6c1de7b140f4
Patrekur <31309672+Patrekur@users.noreply.github.com>
2020-09-27 18:18:15 +0000

    Update context.cpp

--- Project 3 - CANopen machine/src/context.cpp
+++ Project 3 - CANopen machine/src/context.cpp
@@ -29,7 +29,7 @@ class Context {
   ~Context() {
     delete state_;
   }
-
+    
   /**
    * The Context allows changing the State object at runtime.
    */
@@ -60,50 +60,3 @@ class Context {
   }
 
 };
-class ConcreteStateA : public State {
- public:
-   void OnEntry() override {
-    Serial.println("Entering ConcreteStateA.");
-  }
-
-  void OnExit() override {
-    Serial.println("Exiting ConcreteStateA.");
-  }
-
-  void Handle1() override;
-
-  void Handle2() override {
-    Serial.println("ConcreteStateA handles Request2.");
-  }
-
-};
-
-class ConcreteStateB : public State {
- public:
-  void OnEntry() override {
-    Serial.println("Entering ConcreteStateB.");
-  }
-
-  void OnExit() override {
-    Serial.println("Exiting ConcreteStateB.");
-  }
-
-  void Handle1() override {
-    Serial.println("ConcreteStateB handles Request1.");
-  }
-
-  void Handle2() override {
-    Serial.println("ConcreteStateB handles Request2.");
-    Serial.println("ConcreteStateB wants to change the state of the context.");
-    this->context_->TransitionTo(new ConcreteStateA);
-  }
-
-};
-
-void ConcreteStateA::Handle1() {
-  {
-    Serial.println("ConcreteStateA handles Request1.");
-    Serial.println("ConcreteStateA wants to change the state of the context.");
-    this->context_->TransitionTo(new ConcreteStateB);
-  }
-}
