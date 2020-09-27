eb87dde432fab83223270e39c18b96dcaaa70811
eidur84 <58174587+eidur84@users.noreply.github.com>
2020-09-27 17:07:59 +0000

    Timer2 timer settur upp og BSP main fikt

---
+++ Project 3 - CANopen machine/include/Timer.h
@@ -0,0 +1,35 @@
+#ifndef TIMER_H
+#define TIMER_H
+#include <arduino.h>
+
+// Timer that counts seco
+
+class Timer {
+public:
+    
+    Timer(int freq);
+    // initalize the the timer (timer 1)
+    void init();
+    // Start the timer
+    void start();
+    // Stop the timer
+    void stop();
+    // Check if finished
+    bool isFinished();
+    // Change the timeout value
+    void changeTimeout(int freq);
+    // Time increments on each output compare
+    void incrTime();
+
+private:
+
+    // The timeout in seconds
+    int interval = 0;
+    // Value of the time in seconds
+    int timer = 0;
+    // boolean indicating timeout
+    bool finished;
+
+};
+
+#endif
