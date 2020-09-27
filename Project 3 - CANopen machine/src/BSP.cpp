02b8c6b33c2aa23b62051d2c1bcb123b7f7c5683
Patrekur <31309672+Patrekur@users.noreply.github.com>
2020-09-27 23:31:43 +0000

    Update BSP.cpp

--- Project 3 - CANopen machine/src/BSP.cpp
+++ Project 3 - CANopen machine/src/BSP.cpp
@@ -1,17 +1,5 @@
-#include <arduino.h>
-#include "analog_in.h"
-#include "analog_out.h"
-#include "digital_out.h"
-#include "context.h"
-#include "states.h"
-#include "filter.h"
-#include "timer.h"
 
-Digital_out ledOut(4);
-Digital_out ledStatus(5);
-Analog_in input(0);
-Analog_out output(0.5);
-Timer tim(1);
+#include "states.h"
 
 Context *context;
 
@@ -20,9 +8,9 @@ void setup() {
   Serial.begin(9600);
   // Goes into initialization state
   context = new Context(new Initialize);
-  tim.init();
-  ledStatus.init();
-  tim.start();
+  // tim.init();
+  // ledStatus.init();
+  // tim.start();
 }
 
 // Events:
@@ -32,7 +20,9 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+  //delay(500);
 
+  //context->toggleStatus();  
   // Henda þessu inn í State sem housekeeping?
   if(Serial.available())
   {
@@ -45,34 +35,40 @@ void loop() {
       case '2':
         context->Configure();
         break;
+      case '3':
+        context->setOp();
+        break;
+      case '4':
+        context->setPreOp();
+        break;
     }
   }
 }
 
 // Timer 1 used for PWM, used by analog out class
-ISR(TIMER1_COMPB_vect)
-{
+// ISR(TIMER1_COMPB_vect)
+// {
 
-  ledOut.set_lo();
+//   ledOut.set_lo();
 
-}
+// }
 
-ISR(TIMER1_COMPA_vect)
-{
+// ISR(TIMER1_COMPA_vect)
+// {
 
-  ledOut.set_hi();
+//   ledOut.set_hi();
 
-}
+// }
 
-// Status light interrupt
-ISR(TIMER2_COMPA_vect) {
+// // Status light interrupt
+// ISR(TIMER2_COMPA_vect) {
 
-tim.incrTime();
+// tim.incrTime();
 
-if (tim.isFinished()) {
+// if (tim.isFinished()) {
 
-  ledStatus.toggle();
+//   ledStatus.toggle();
 
-}
+// }
 
-}
+// }
